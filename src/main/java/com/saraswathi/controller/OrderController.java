35bce02e387b40a6bb08369f04ff807efe7eb7cb
Saraswathikoratla <saraswathi.koratla904@gmail.com>
2026-04-12 08:13:38 +0530

    initial commit

---
+++ src/main/java/com/saraswathi/controller/OrderController.java
@@ -0,0 +1,41 @@
+package com.saraswathi.controller;
+
+import com.saraswathi.dto.OrderRequest;
+import com.saraswathi.dto.Product;
+import com.saraswathi.service.ProductClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+    @GetMapping("/all")
+    public String getOrders(
+            @RequestHeader("X-User") String user,
+            @RequestHeader(value = "X-Role", required = false) String role) {
+        System.out.println("role.."+role);
+        return "Orders for user: " + user + " | Role: " + role;
+    }
+    @Autowired
+    private ProductClient productClient;
+
+    @PostMapping
+    public String placeOrder(
+            @RequestHeader("X-User") String user,
+            @RequestBody OrderRequest request) {
+
+        Product product = productClient.getProductById(request.getProductId());
+
+        if (product == null) {
+            return "Product not found";
+        }
+
+        if (product.getQuantity() < request.getQuantity()) {
+            return "Insufficient stock";
+        }
+
+        return "Order placed by " + user + " for " + product.getName();
+    }
+
+}
---
+++ src/main/java/com/saraswathi/service/OrderService.java
@@ -0,0 +1,10 @@
+package com.saraswathi.service;
+
+import com.saraswathi.dto.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+
+    }
